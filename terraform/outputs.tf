cd4313f860ec8a28b319bb64707195905445387e
devanshutyagi120101 <167014960+devanshutyagi120101@users.noreply.github.com>
2024-06-09 18:59:41 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.main.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.main.name
+}
+output "ecs_task_definition_arn" {
+  value = aws_ecs_task_definition.main.arn
+}
+
